3a8c9c4520dee01ca40acbe5878c7c803d7e6931
Tim <tim.barden@junipereducation.org>
2023-05-01 09:37:07 +0100

    Modify header styling based homepage/inner

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-      @entries = Entry.search(params[:search])
+    @homepage = true
+    @entries = Entry.search(params[:search])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
     include Pagy::Frontend
+
+    def homepage
+        return current_page?(root_url)
+    end
 end
