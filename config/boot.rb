189970e5bea399b325aba4860883fd34aa51040f
Tim <tim.barden@junipereducation.org>
2023-05-06 18:30:34 +0100

    add extras to complete simplecov setup with rspec

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,8 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+if ENV['ENABLE_CODE_COVERAGE']
+    require 'simplecov'
+    SimpleCov.start 'rails'
+end
