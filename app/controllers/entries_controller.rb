79b46a4802aa4309efb385ba428b37cc6728f5ee
Tim <tim.barden@junipereducation.org>
2023-05-06 09:36:04 +0100

    Add pagination to entries search results and adjust pagy count on Entries page

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,7 +4,7 @@ class EntriesController < ApplicationController
   # GET /entries or /entries.json
   def index
     @entries = Entry.all.order("created_at DESC")
-    @pagy, @entries = pagy(Entry.all, items: 3)
+    @pagy, @entries = pagy(Entry.all, items: 10)
   end
 
   # GET /entries/1 or /entries/1.json
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   def index
     @homepage = true
     @entries = Entry.search(params[:search])
+    if @entries
+      @pagy, @entries = pagy(Entry.search(params[:search]), items: 10)
+    end
   end
 end
