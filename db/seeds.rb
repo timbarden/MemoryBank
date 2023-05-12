36c9bbbcffc3e07137e17e8cbfe80e18c2d20a35
Tim <tim.barden@junipereducation.org>
2023-05-12 20:43:36 +0100

    Add seed setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+User.create(name: "Test User", email: ENV["TEST_EMAIL_ADDRESS"], password: ENV["TEST_PASSWORD"])
+
+x = 0
+while x < 10
+    Entry.create(question: "This is example question number #{x+1}", answer: "This is the answer for example question #{x+1}", user_id: 1)
+    x += 1
+end
