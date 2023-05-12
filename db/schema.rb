18f0e4ee4407b795d601d9bd27dc52211abe2b77
Tim <tim.barden@junipereducation.org>
2023-05-12 21:01:30 +0100

    add tags table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_28_110041) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_12_195640) do
   create_table "entries", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -18,6 +18,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_28_110041) do
     t.text "answer"
     t.integer "user_id"
     t.index ["user_id"], name: "index_entries_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
